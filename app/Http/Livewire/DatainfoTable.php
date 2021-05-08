1b909b2f2852b8b04b8e2f8a5757e13061c1fa62
Sebastian Roldan <sroldan@herfox.com>
2021-05-08 14:41:45 -0500

    Funcionalidad del Filtro

--- app/Http/Livewire/DatainfoTable.php
+++ app/Http/Livewire/DatainfoTable.php
@@ -11,10 +11,12 @@ class DatainfoTable extends Component
     use WithPagination;
 
     protected $queryString = [
-        'searchName' => ['except' => ''],
-        'searchLast' => ['except' => ''],
-        'searchLocation' => ['except' => ''],
-        'searchCity' => ['except' => ''],
+        'searchName'        => ['except' => ''],
+        'searchLast'        => ['except' => ''],
+        'searchLocation'    => ['except' => ''],
+        'searchCity'        => ['except' => ''],
+        // 'searchGender'      => ['except' => ''],
+        // 'searchCivil'       => ['except' => ''],
         'perPage'
     ];
 
@@ -22,6 +24,8 @@ class DatainfoTable extends Component
     public $searchLast = '';
     public $searchLocation = '';
     public $searchCity = '';
+    // public $searchGender = '';
+    // public $searchCivil = '';
     public $perPage = 5;
 
     public function render()
@@ -31,6 +35,8 @@ class DatainfoTable extends Component
                 ->where('apellido', 'LIKE', "%{$this->searchLast}%")
                 ->where('ubicacion', 'LIKE', "%{$this->searchLocation}%")
                 ->where('ciudad', 'LIKE', "%{$this->searchCity}%")
+                // ->where('genero', $this->searchGender)
+                // ->where('civil', $this->searchCivil)
                 ->paginate($this->perPage)
         ])->layout('layouts.app');
     }
@@ -43,10 +49,5 @@ class DatainfoTable extends Component
         $this->searchCity = '';
         $this->page = 1;
         $this->perPage = 5;
-    }
-
-    public function directHome()
-    {
-        
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -29,5 +29,24 @@ Route::middleware(['auth:sanctum', 'verified'])
 
 Route::middleware(['auth:sanctum', 'verified'])
     ->post('/colombiaSearch', function (Request $request) {
-        dd($request->all());
+        $name = (strlen($request->primer_nombre) > 0) ? $request->primer_nombre : null;
+        if (is_null($name)) {
+            $name = (strlen($request->segundo_nombre) > 0) ? $request->segundo_nombre : '';
+        } else {
+            $name = (strlen($request->segundo_nombre) > 0) ? $name . ' ' . $request->segundo_nombre : $name;
+        }
+
+        $nameLast = (strlen($request->primer_apellido) > 0) ? $request->primer_apellido : null;
+        if (is_null($nameLast)) {
+            $nameLast = (strlen($request->segundo_apellido) > 0) ? $request->segundo_apellido : '';
+        } else {
+            $nameLast = (strlen($request->segundo_apellido) > 0) ? $nameLast . ' ' . $request->segundo_apellido : $nameLast;
+        }
+
+        $gender = (is_null($request->genero)) ? '' : $request->genero;
+        $city = (strlen($request->ciudad) > 0) ? $request->ciudad : '';
+        $location = (strlen($request->ubicacion) > 0) ? $request->ubicacion : '';
+        $civil = (is_null($request->civil)) ? '' : $request->civil;
+
+        return redirect('/colombia?perPage=5&searchName='.$name.'&searchLast='.$nameLast.'&searchLocation='.$location.'&searchCity='.$city.'');
     });
